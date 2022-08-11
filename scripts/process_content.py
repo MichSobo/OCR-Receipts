d62377602ca1ea3bb2547f2638441047a1703f69
MichSobo <michsob24@gmail.com>
2022-08-11 22:02:42 +0200

    Set file paths

--- scripts/process_content.py
+++ scripts/process_content.py
@@ -2,3 +2,9 @@
 Script for extracting relevant data from text that was recognized based on
 receipt image.
 """
+import os
+from pathlib import Path
+
+# Set input file path
+txt_content_filename = 'Paragon_2022-08-11_081131_300dpi.txt'
+txt_content_filepath = Path('results') / txt_content_filename
