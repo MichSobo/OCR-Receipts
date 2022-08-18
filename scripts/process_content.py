1f16c2134555d9cab33c11ace7b8cdf4a172b2e0
MichSobo <michsob24@gmail.com>
2022-08-18 22:54:54 +0200

    Update script for processing content

--- scripts/process_content.py
+++ scripts/process_content.py
@@ -7,13 +7,17 @@ import os
 import json
 from pathlib import Path
 
-# Set input file path
+
+# Set input file name
 content_filename = Path('Paragon_2022-08-11_081131_300dpi.txt')
-content_filepath = Path('../results') / content_filename.stem / content_filename
+content_filepath = Path('../results') / content_filename.stem / 'content.txt'
+
+# Set output folder path
+out_folderpath = content_filepath.parent
 
 
 def get_shop(text):
-    """Extract shop name from receipt text."""
+    """Extract shop name from receipt's raw content."""
     SHOPS = {
         'Biedronka': ('biedronka',),
         'Rossmann': ('rossmann',),
@@ -28,36 +32,40 @@ def get_shop(text):
 
 content = {}
 
-# Read text file content
+# Read text file
 with open(content_filepath, 'r', encoding='utf-8') as f:
     text = f.readlines()
 
 # Extract shop name
 content['shop'] = get_shop(text)
 
-# Extract all products with quantity and prices
+# Define regex for data extraction
 product_regex = re.compile(r'''(
     (.+)            # product
     \s+\S{1,2}\s+   # char surrounded with spaces
     (\d([,.]\d+)?)  # quantity
-    \s*             # 0 or more spaces
-    [xX]            # "x"
-    (\d+[,.]\d{2})  # product price
+    \s*             # space
+    \S{1,2}         # "x"
+    (\d+,\s?\d{2})  # product price
     \s+             # space
-    (\d+[,.]\d{2})  # total sum for product
+    (\d+,\s?\d{2})  # total sum for product
 )''', re.VERBOSE)
 
-# Replace wrong character
+# Replace common wrong characters
 text = list(map(lambda x: x.replace('{', '1'), text))
+text = list(map(lambda x: x.replace('(', '1'), text))
 content['products'] = []
 
+# Define lists for storing products
+raw_products = []
 products = []
 
+# Extract all products with quantity and prices
 for row in text:
     match = re.search(product_regex, row)
     if match:
         groups = match.groups()
-        products.append(groups[0])
+
         content['products'].append({
             'name': groups[1],
             'quantity': groups[2],
@@ -65,8 +73,21 @@ for row in text:
             'total_price': groups[5]
         })
 
+        raw_products.append(groups[0])
+
+        products.append(
+            " ".join([groups[1], groups[2], 'x' + groups[4], groups[5]])
+        )
+
+# Write raw products to text file
+products_filename = 'raw_products.txt'
+products_filepath = out_folderpath / products_filename
+with open(products_filepath, 'w', encoding='utf-8') as f:
+    f.write('\n'.join(raw_products))
+
+# Write products to text file
 products_filename = 'products.txt'
-products_filepath = content_filepath.parent / products_filename
+products_filepath = out_folderpath / products_filename
 with open(products_filepath, 'w') as f:
     f.write('\n'.join(products))
 
@@ -75,16 +96,8 @@ total_cost_regex = re.compile(r'SUMA\s+\w+\s+(\d+[,.]\d+)')
 match = re.search(total_cost_regex, ''.join(list(text)))
 content['total_sum'] = match.group(1)
 
-# Define a regex pattern for price
-price_regex = r'\d+\.\d+'
-
-with open('res.json', 'w') as f:
+# Write extracted content to json file
+json_content_filename = 'content.json'
+json_content_filepath = out_folderpath / json_content_filename
+with open(json_content_filepath, 'w') as f:
     json.dump(content, f, indent=4)
-
-# TODO: Write all extracted products to file (content.txt)
-# TODO: Write all extracted products to file (content.json)
-# TODO: Build test suite to enhance the extraction process
-# TODO: Check that qty * product price = total for product
-# TODO: Check that all products - discounts = total sum
-# 1 is wrongly taken as 4
-# 1 is wrongly taken as {
