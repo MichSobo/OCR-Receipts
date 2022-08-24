7b64aad7c72e517d1628b6a641149b4c01c6b997
MichSobo <michsob24@gmail.com>
2022-08-24 08:39:21 +0200

    Refactor code

--- scripts/process_content.py
+++ scripts/process_content.py
@@ -46,6 +46,66 @@ def get_shop(text, str_if_unrecognized='unknown'):
     return str_if_unrecognized
 
 
+def get_products(text):
+    """Return products."""
+    # Define regex for product data extraction
+    product_regex = re.compile(r'''(
+            (.+)            # product
+            \s+\S{1,2}\s+   # char surrounded with spaces
+            (\d([,.]\d+)?)  # quantity
+            \s*             # space
+            \S{1,2}         # "x"
+            (\d+,\s?\d{2})  # product price
+            \s+             # space
+            (\d+,\s?\d{2})  # total sum for product
+        )''', re.VERBOSE)
+
+    raw_products_str = []
+    products_str = []
+    products = []
+
+    match = re.search(product_regex, text)
+    if match:
+        groups = match.groups()
+
+        raw_products_str.append(groups[0])
+
+        products_str.append(
+            " ".join([groups[1], groups[2], 'x' + groups[4], groups[5]])
+        )
+
+        products.append({
+            'name': groups[1],
+            'quantity': groups[2],
+            'price': groups[4],
+            'total_price': groups[5]
+        })
+
+        # Write raw products to text file
+        products_filename = 'raw_products.txt'
+        products_filepath = out_folderpath / products_filename
+        with open(products_filepath, 'w', encoding='utf-8') as f:
+            f.write('\n'.join(raw_products))
+        print(f'Raw products were written to file "{products_filepath}"')
+
+        # Write products to text file
+        products_filename = 'products.txt'
+        products_filepath = out_folderpath / products_filename
+        with open(products_filepath, 'w') as f:
+            f.write('\n'.join(products))
+        print(f'Processed products were written to file "{products_filepath}"')
+
+        return products
+
+
+def get_total(text):
+    """Return total sum."""
+    total_cost_regex = re.compile(r'SUMA\s+\w+\s+(\d+[,.]\d+)')
+    match = re.search(total_cost_regex, ''.join(list(text)))
+
+    return match.group(1)
+
+
 content = {}
 
 # Read text file
@@ -56,64 +116,15 @@ print(f'Text content was read from file "{content_filepath}"')
 # Extract shop name
 content['shop'] = get_shop(text)
 
-# Define regex for data extraction
-product_regex = re.compile(r'''(
-    (.+)            # product
-    \s+\S{1,2}\s+   # char surrounded with spaces
-    (\d([,.]\d+)?)  # quantity
-    \s*             # space
-    \S{1,2}         # "x"
-    (\d+,\s?\d{2})  # product price
-    \s+             # space
-    (\d+,\s?\d{2})  # total sum for product
-)''', re.VERBOSE)
-
 # Replace common wrong characters
 text = list(map(lambda x: x.replace('{', '1'), text))
 text = list(map(lambda x: x.replace('(', '1'), text))
-content['products'] = []
 
 # Define lists for storing products
-raw_products = []
-products = []
-
-# Extract all products with quantity and prices
-for row in text:
-    match = re.search(product_regex, row)
-    if match:
-        groups = match.groups()
-
-        content['products'].append({
-            'name': groups[1],
-            'quantity': groups[2],
-            'price': groups[4],
-            'total_price': groups[5]
-        })
-
-        raw_products.append(groups[0])
-
-        products.append(
-            " ".join([groups[1], groups[2], 'x' + groups[4], groups[5]])
-        )
-
-# Write raw products to text file
-products_filename = 'raw_products.txt'
-products_filepath = out_folderpath / products_filename
-with open(products_filepath, 'w', encoding='utf-8') as f:
-    f.write('\n'.join(raw_products))
-print(f'Raw products were written to file "{products_filepath}"')
-
-# Write products to text file
-products_filename = 'products.txt'
-products_filepath = out_folderpath / products_filename
-with open(products_filepath, 'w') as f:
-    f.write('\n'.join(products))
-print(f'Processed products were written to file "{products_filepath}"')
+content['products'] = get_products(text)
 
 # Extract total cost
-total_cost_regex = re.compile(r'SUMA\s+\w+\s+(\d+[,.]\d+)')
-match = re.search(total_cost_regex, ''.join(list(text)))
-content['total_sum'] = match.group(1)
+content['total_sum'] = get_total(text)
 
 # Write extracted content to json file
 json_content_filename = 'content.json'
