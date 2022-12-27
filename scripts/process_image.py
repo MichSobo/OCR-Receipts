d024c8ff5cef9611612a05c078a2881ce7b7dc86
MichSobo <michsob24@gmail.com>
2022-12-27 21:11:43 +0100

    Quick fixes

--- scripts/process_image.py
+++ scripts/process_image.py
@@ -4,7 +4,7 @@ Code for image processing and retrieving content with use of OCR methods.
 import os
 import functools
 
-import cv2 as cv
+import cv2
 import imutils
 import pytesseract
 from imutils.perspective import four_point_transform
@@ -38,8 +38,8 @@ def debug_image(func):
         if debug_mode is True:
             # Show the processed image during function execution
             proc_img_name = proc_img_name_mapper[func.__name__].capitalize()
-            cv.imshow(proc_img_name, proc_img)
-            cv.waitKey(0)
+            cv2.imshow(proc_img_name, proc_img)
+            cv2.waitKey(0)
 
         if save_proc_img is True:
             # Get output folder path if passed else get default
@@ -49,7 +49,7 @@ def debug_image(func):
             proc_img_filename = proc_img_name_mapper[func.__name__] + '.jpg'
             proc_img_filepath = os.path.join(proc_img_folderpath, proc_img_filename)
 
-            cv.imwrite(proc_img_filepath, proc_img)
+            cv2.imwrite(proc_img_filepath, proc_img)
 
             if LOG:
                 print(f'Image was saved to file "{proc_img_filepath}"')
@@ -64,7 +64,7 @@ def read_image(path):
     if not os.path.isfile(path):
         raise FileNotFoundError(f'No such file: "{path}"')
 
-    img = cv.imread(path)
+    img = cv2.imread(path)
 
     if LOG:
         print(f'Image was read from file "{path}"')
@@ -92,9 +92,9 @@ def resize_image(img, debug=False, save_proc_img=False, proc_img_folderpath=None
 @debug_image
 def adjust_color(img, debug=False, save_proc_img=False, proc_img_folderpath=None):
     """Return an image with adjusted color to enhance contour detection."""
-    img_grayed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)       # convert to grayscale
-    img_blurred = cv.GaussianBlur(img_grayed, (5, 5,), 0)  # blur using Gaussian kernel
-    img_edged = cv.Canny(img_blurred, 75, 200)             # apply edge detection
+    img_grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)       # convert to grayscale
+    img_blurred = cv2.GaussianBlur(img_grayed, (5, 5,), 0)  # blur using Gaussian kernel
+    img_edged = cv2.Canny(img_blurred, 75, 200)             # apply edge detection
 
     return img_edged
 
@@ -103,7 +103,7 @@ def adjust_color(img, debug=False, save_proc_img=False, proc_img_folderpath=None
 def draw_outline(img, contour, debug=False, save_proc_img=False, proc_img_folderpath=None):
     """Return an image with added contour layer."""
     img_outlined = img.copy()
-    cv.drawContours(img_outlined, [contour], -1, (0, 255, 0), 2)
+    cv2.drawContours(img_outlined, [contour], -1, (0, 255, 0), 2)
 
     return img_outlined
 
@@ -111,21 +111,21 @@ def draw_outline(img, contour, debug=False, save_proc_img=False, proc_img_folder
 def get_contour(img):
     """Return a list of contours found in image's edge map."""
     # Find contours
-    contours = cv.findContours(
-        img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
-    )
+    contours = cv2.findContours(img,
+                                cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
     # Sort contours according to their area size
-    contours = sorted(contours, key=cv.contourArea, reverse=True)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
     # Initialize a variable to store the contour
     contour = None
 
     for c in contours:
         # Approximate the contour by reducing the number of points
-        peri = cv.arcLength(c, True)
-        approx = cv.approxPolyDP(c, 0.02 * peri, True)
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
         # If the approximated contour has 4 points...
         if len(approx) == 4:
@@ -146,7 +146,7 @@ def transform_image(img, contour, debug=False, save_proc_img=False, proc_img_fol
 
     Arguments:
         img (object): image to be transformed
-        contour (list): contour definition
+        contour (object): contour definition
 
     Returns:
         object: transformed image
@@ -187,7 +187,7 @@ def recognize_content(img,
             line of the recognized content
     """
     # Execute OCR
-    text = pytesseract.image_to_string(cv.cvtColor(img, cv.COLOR_BGR2RGB),
+    text = pytesseract.image_to_string(cv2.cvtColor(img, cv2.COLOR_BGR2RGB),
                                        config='--psm 4')
 
     if write_content is True:
